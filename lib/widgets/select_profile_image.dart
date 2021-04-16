c5394d9e51b8a6197cc9c23d8dac383815025567
Taylan YILDIZ <taylanyildz@outlook.com>
2021-04-16 18:35:38 +0300

    Update select_profile_image.dart

--- lib/widgets/select_profile_image.dart
+++ lib/widgets/select_profile_image.dart
@@ -1,9 +1,31 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:select_profile_photo/select_profile_photo.dart';
 
 class ProfileImage extends StatelessWidget {
   var imagesFile = <File>[];
+  AndroidUiSettings androidUiSettings() {
+    return AndroidUiSettings(
+      toolbarColor: Colors.red,
+      toolbarWidgetColor: Colors.white,
+      toolbarTitle: 'Profilin İçin Foğoraf seç',
+      activeControlsWidgetColor: Colors.white,
+      lockAspectRatio: false,
+      hideBottomControls: true,
+    );
+  }
+
+  IOSUiSettings iosUiSettings() {
+    return IOSUiSettings(
+      doneButtonTitle: 'Seç',
+      cancelButtonTitle: 'Çık',
+      rotateButtonsHidden: true,
+      rotateClockwiseButtonHidden: true,
+      title: 'Profilin için Fotoğraf seç',
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +47,7 @@ class ProfileImage extends StatelessWidget {
           ImagePicker(
             itemCount: 3,
             height: 120.0,
+            androidUiSettingsLock: androidUiSettings(),
             backgroundColor: Colors.black,
             backgroundImage: Colors.white,
             iconColor: Colors.red,
